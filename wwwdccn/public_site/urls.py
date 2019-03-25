6a91e12a6f1d43792368ca71543b985ca116b4ff
Andrey Larionov <larioandr@gmail.com>
2019-03-25 23:46:43 +0300

    Add landing draft (no translations, bad responsibility, lorem ipsum for about and prev events, no links)

--- wwwdccn/public_site/urls.py
+++ wwwdccn/public_site/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='public_site/index.html'), name='landing'),
+    path('', TemplateView.as_view(template_name='public_site/index.html'), name='public_index'),
+    path('', TemplateView.as_view(template_name='public_site/index.html'), name='home'),
 ]
