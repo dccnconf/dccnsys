3308caccdec08fa6d69f59302a59aac5bf0a6d95
Andrey Larionov <larioandr@gmail.com>
2019-05-29 23:54:43 +0300

    Add custom select multiple widget with checkbox with correct slim rendering with Bootstrap4

--- wwwdccn/gears/widgets.py
+++ wwwdccn/gears/widgets.py
@@ -1,7 +1,11 @@
-from django.forms import FileInput
+from django.forms import FileInput, CheckboxSelectMultiple
 
 
 class CustomFileInput(FileInput):
     template_name = 'gears/widgets/file_input.html'
     accept = ''
     show_file_name = True
+
+
+class CustomCheckboxSelectMultiple(CheckboxSelectMultiple):
+    template_name = 'gears/widgets/checkbox_multiple_select.html'
