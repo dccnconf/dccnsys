28aa3c13ae8f12ca41566a4afacf8a86b12e3d51
Andrey Larionov <larioandr@gmail.com>
2019-07-16 21:38:46 +0300

    Extract models for email templates and email messages, add email settings model

--- wwwdccn/chair_mail/models.py
+++ wwwdccn/chair_mail/models.py
@@ -4,8 +4,33 @@ from conferences.models import Conference
 from users.models import User
 
 
-class EmailTemplate(models.Model):
+class HtmlEmailTemplate(models.Model):
     name = models.CharField(max_length=1024, blank=False)
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    conference = models.ForeignKey(Conference, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("name", "conference"),)
+
+
+class TextEmailTemplate(models.Model):
+    name = models.CharField(max_length=1024, blank=False)
+    body = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    conference = models.ForeignKey(Conference, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("name", "conference"),)
+
+
+class EmailMessage(models.Model):
+    name = models.CharField(max_length=1024, blank=False)
+    html = models.TextField()
     text = models.TextField()
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now_add=True)
@@ -23,3 +48,10 @@ class EmailUserList(models.Model):
 
     class Meta:
         unique_together = (("name", "conference"),)
+
+
+class EmailGeneralSettings(models.Model):
+    footer_html = models.TextField()
+    footer_text = models.TextField()
+    sign_footer_html = models.TextField()
+    sign_footer_text = models.TextField()
