1518e83db5dee1eda0bcc91634792dc11941c31e
Andrey Larionov <larioandr@gmail.com>
2019-08-26 21:25:51 +0300

    Refactor review post_save and post_delete events by extracting _send_email from them

--- wwwdccn/review/models.py
+++ wwwdccn/review/models.py
@@ -140,53 +140,39 @@ def check_review_details(value, submission_type):
     return num_words >= submission_type.min_num_words_in_review
 
 
-@receiver(post_save, sender=Review)
-def create_review(sender, instance, created, **kwargs):
-    if created:
-        assert isinstance(instance, Review)
-        user = instance.reviewer.user
-        profile = user.profile
-        context = {
-            'email': user.email,
-            'first_name': profile.first_name,
-            'last_name': profile.last_name,
-            'review': instance,
-            'protocol': settings.SITE_PROTOCOL,
-            'domain': settings.SITE_DOMAIN,
-            'deadline': instance.paper.conference.review_stage.end_date,
-        }
-        html = render_to_string('review/email/start_review.html', context)
-        text = render_to_string('review/email/start_review.txt', context)
-        send_mail(
-            f"[DCCN2019] Review assignment for submission #{instance.paper.pk}",
-            message=text,
-            html_message=html,
-            recipient_list=[user.email],
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            fail_silently=False,
-        )
-
-
-@receiver(post_delete, sender=Review)
-def delete_review(sender, instance, **kwargs):
-    user = instance.reviewer.user
+def _send_email(user, review, subject, template_html, template_plain):
     profile = user.profile
     context = {
         'email': user.email,
         'first_name': profile.first_name,
         'last_name': profile.last_name,
-        'review': instance,
+        'review': review,
         'protocol': settings.SITE_PROTOCOL,
         'domain': settings.SITE_DOMAIN,
-        'deadline': instance.paper.conference.review_stage.end_date,
+        'deadline': review.paper.conference.review_stage.end_date,
     }
-    html = render_to_string('review/email/cancel_review.html', context)
-    text = render_to_string('review/email/cancel_review.txt', context)
-    send_mail(
+    html = render_to_string(template_html, context)
+    text = render_to_string(template_plain, context)
+    send_mail(subject, message=text, html_message=html,
+              recipient_list=[user.email],
+              from_email=settings.DEFAULT_FROM_EMAIL, fail_silently=False)
+
+
+@receiver(post_save, sender=Review)
+def create_review(sender, instance, created, **kwargs):
+    if created:
+        assert isinstance(instance, Review)
+        _send_email(
+            instance.reviewer.user, instance,
+            f"[DCCN2019] Review assignment for submission #{instance.paper.pk}",
+            template_html='review/email/start_review.html',
+            template_plain='review/email/start_review.txt')
+
+
+@receiver(post_delete, sender=Review)
+def delete_review(sender, instance, **kwargs):
+    _send_email(
+        instance.reviewer.user, instance,
         f"[DCCN2019] Review cancelled for submission #{instance.paper.pk}",
-        message=text,
-        html_message=html,
-        recipient_list=[user.email],
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        fail_silently=False,
-    )
+        template_html='review/email/cancel_review.html',
+        template_plain='review/email/cancel_review.txt')
