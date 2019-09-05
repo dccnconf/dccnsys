9e86de8d9b61f3a51b3c4bdb320ee08d7d583051
Andrey Larionov <larioandr@gmail.com>
2019-09-05 15:52:41 +0300

    Remove no-country option from export dialogs

--- wwwdccn/chair/views/export.py
+++ wwwdccn/chair/views/export.py
@@ -1,7 +1,6 @@
 import csv
 from datetime import datetime
 
-from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
